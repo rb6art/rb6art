e13ba4e38ea2baad4e46542a5e64a6be48a59ede
Chris Whitehead <cdub@MacBook-Pro.attlocal.net>
2021-02-24 17:38:57 -0500

    Adding tests for; signup, signin, signout and currentUser routes.

---
+++ auth/src/routeControllers/__test__/signin.test.ts
@@ -0,0 +1,62 @@
+import request from "supertest";
+import { app } from '../../app';
+
+const postSigninURL: string = '/api/user/signin';
+const postSignupURL: string = '/api/user/signup';
+
+describe('POST api/user/signin', () => {
+
+  it('should return a 400 if no user with that email is found', async () => {
+    await request(app)
+      .post(postSigninURL)
+      .send({
+        email: "test@test.com",
+        password: "password"
+      })
+      .expect(400);
+  });
+
+
+  it('should return a 400 if the password provided does not match hashed password', async () => {
+
+    await request(app)
+      .post(postSignupURL)
+      .send({
+        email: "test@test.com",
+        password: "password"
+      })
+      .expect(201);
+
+    await request(app)
+      .post(postSigninURL)
+      .send({
+        email: "test@test.com",
+        password: "asdfas"
+      })
+      .expect(400);
+
+  });
+
+  it('should set a cookie with Set-Cookie header when credientials are valid', async () => {
+
+    await request(app)
+      .post(postSignupURL)
+      .send({
+        email: "test@test.com",
+        password: "password"
+      })
+      .expect(201);
+      
+    const res = await request(app)
+      .post(postSigninURL)
+      .send({
+        email: "test@test.com",
+        password: "password"
+      })
+      .expect(201);
+    
+    expect(res.get('Set-Cookie')).toBeDefined();
+    
+  });
+ 
+});
--- auth/src/routeControllers/__test__/signup.test.ts
+++ auth/src/routeControllers/__test__/signup.test.ts
@@ -1,11 +1,14 @@
 import request from "supertest";
 import { app } from '../../app';
 
+const postSignupURL: string = '/api/user/signup';
+
 describe('POST api/user/signup', () => {
 
-  it ('should return a 201 on a seccessful signup', async () => {
-    return request(app)
-      .post('/api/user/signup')
+  it('should return a 201 on a seccessful signup', async () => {
+
+    await request(app)
+      .post(postSignupURL)
       .send({
         email: "test@test.com",
         password: "234jkhg4k1j4h"
@@ -13,4 +16,77 @@ describe('POST api/user/signup', () => {
       .expect(201);
   });
 
-})
+  it('should return a 400 when EMAIL validation fails', async () => {
+
+    await request(app)
+      .post(postSignupURL)
+      .send({
+        email: "tetest.com",
+        password: "password"
+      })
+      .expect(400);
+  });
+
+  it('should return a 400 when PASSWORD validation fails', async () => {
+
+    await request(app)
+      .post(postSignupURL)
+      .send({
+        email: "tet@est.com",
+        password: "24h"
+      })
+      .expect(400);
+  });
+
+  it('should return a 400 when email and password is missing', async () => {
+
+    await request(app)
+      .post(postSignupURL)
+      .send({
+        email: "test@test.com",
+        password: ""
+      })
+      .expect(400);
+
+    await request(app)
+      .post(postSignupURL)
+      .send({
+        email: "",
+        password: "password"
+      })
+      .expect(400);
+  });
+
+  it('should not allow dublicate emails', async () => {
+
+    await request(app)
+      .post(postSignupURL)
+      .send({
+        email: "test@test.com",
+        password: "234jkhg4k1j4h"
+      })
+      .expect(201);
+
+    await request(app)
+      .post(postSignupURL)
+      .send({
+        email: "test@test.com",
+        password: "234jkhg4k1j4h"
+      })
+      .expect(400);
+  });
+
+  it('should have a header of Set-Cookie after setting the jwt', async () => {
+
+    const res = await request(app)
+      .post(postSignupURL)
+      .send({
+        email: "test@test.com",
+        password: "234jkhg4k1j4h"
+      })
+      .expect(201);
+    
+    expect(res.get('Set-Cookie')).toBeDefined();
+
+  });
+});
