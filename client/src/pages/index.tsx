2c6d9d312f1a95b2facf9f740768b07ed410b897
Chris Whitehead <cdub@MacBook-Pro.attlocal.net>
2021-02-27 18:32:40 -0500

    Cleaning up the theme toggle buttons styles.

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,16 +1,10 @@
 import { Hero } from '../components/Hero'
 import { Container } from '../components/base/Container'
-// import { DarkModeSwitch } from '../components/DarkModeSwitch'
 
 const Index = () => (
   <Container height="100vh">
     <Hero />
-    
-    
     This is where all the shit will go..
-    
-
-    {/* <DarkModeSwitch /> */}
   </Container>
 )
 
