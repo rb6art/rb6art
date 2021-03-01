1c67c3d55aae58dc9963dcd92282cd507fa5597c
Chris Whitehead <cdub@MacBook-Pro.attlocal.net>
2021-03-01 12:17:44 -0500

    Page flow fix. Allowing for a full background image behind he hero and feed component

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,11 +1,35 @@
 import { Hero } from '../components/Hero'
 import { Container } from '../components/base/Container'
+import { Box, Flex } from '@chakra-ui/react'
+import styled from '@emotion/styled'
+
+const height = '90vh'
+const BackgroundWrapper = styled.div`
+  padding-top: 100px;
+  height: ${height};
+  position: absolute;
+  width: 100%;
+  background: teal;
+  text-align: center;
+  z-index: -1;
+`
 
 const Index = () => (
-  <Container height="100vh">
-    <Hero />
-    This is where all the shit will go..
-  </Container>
+  <div>
+    <BackgroundWrapper>
+      The teal background will be replaced with a video
+    </BackgroundWrapper>
+    <Container height="100%">
+      <Flex direction="column" h={height} w="100%">
+        <Box mr="20" ml="20" h="100vh">
+          <Hero />
+          <Box bg="red" textAlign="center">
+            Will show redit rb6 feed..
+          </Box>
+        </Box>
+      </Flex>
+    </Container>
+  </div>
 )
 
 export default Index
