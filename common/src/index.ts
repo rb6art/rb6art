bde1910b2c8df545bfb5884ba87bc2c21253e25e
Chris Whitehead <cdub@MacBook-Pro.attlocal.net>
2021-03-02 22:12:16 -0500

    Adding the start of the common module.. now that its working correctly

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,11 +1,7 @@
-export * from './errors/bad-request-error';
-export * from './errors/custom-error';
-export * from './errors/database-connection-error';
-export * from './errors/not-authorized-error';
-export * from './errors/not-found-error';
-export * from './errors/request-validation-error';
+interface Color {
+  red: number
+}
 
-export * from './middlewares/current-user';
-export * from './middlewares/error-handler';
-export * from './middlewares/require-auth';
-export * from './middlewares/validate-request';
+export const color = {
+  red: 19
+}
