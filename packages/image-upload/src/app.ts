1f83b54628f68aaf3a69d06d4337330e0a85bc6d
Chris Whitehead <cdub@MacBook-Pro.attlocal.net>
2021-04-06 17:11:15 -0400

    Getting the front-end to communicate with backend image upload service.

--- packages/image-upload/src/app.ts
+++ packages/image-upload/src/app.ts
@@ -1,19 +1,19 @@
-import express from 'express';
+import express from 'express'
 import cookieSession from 'cookie-session'
-import 'express-async-errors';
+import 'express-async-errors'
 import { json } from 'body-parser'
-import { NotFoundError, errorHandler, currentUser } from '@rb6art/common';
-import { imageUploadRoutes } from './routes/image-upload-routes';
+import { NotFoundError, errorHandler, currentUser } from '@rb6art/common'
+import { imageUploadRoutes } from './routes/image-upload-routes'
 
-const app = express();
+const app = express()
 
 /**
   We need this because  we are using ingress nginx proxy. 
   By default express will trust traffic even though its coming form that proxy.
 */
-app.set('trust proxy', true);
+app.set('trust proxy', true)
 
-app.use(json());
+app.use(json())
 app.use(
   cookieSession({
     signed: false,
@@ -22,21 +22,21 @@ app.use(
       Supertest uses http request and not https.
       If test is true secure is false..
      */
-    secure: process.env.NODE_ENV !== 'test'
-  })
-);
+    secure: process.env.NODE_ENV !== 'test',
+  }),
+)
 
 // This middleware will check if the currentUser prop is on the res object
 // if not it will throw an error..
-app.use(currentUser);
+app.use(currentUser)
 
 // Middleware Routes
-app.use('/api', imageUploadRoutes);
+app.use('/api', imageUploadRoutes)
 
 app.all('*', async () => {
-  throw new NotFoundError();
-});
+  throw new NotFoundError()
+})
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-export { app };
+export { app }
--- packages/image-upload/src/routeControllers/upload-image.ts
+++ packages/image-upload/src/routeControllers/upload-image.ts
@@ -1,31 +1,33 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from 'express'
 import { Image } from '../model/image'
 
-export const uploadImage = async (req: Request, res: Response, next: NextFunction) => {
+export const uploadImage = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const { description } = req.body
+    const { path, mimetype } = req.file
 
+    console.log(description)
+    console.log(req.file)
 
-console.log(req.file)
-  // try {
-  // const { title, description } = req.body;
-  // const { path, mimetype } = req.file;
+    // // console.log(fs.readFileSync(join(__dirname, '..', path)))
+    // // console.log(join(__dirname, '..', path))
 
-  // console.log(title)
-  // console.log(description)
-  // console.log(req.file)
+    // const file = new Image({
+    //   title,
+    //   description,
+    //   file_path: path,
+    //   file_mimetype: mimetype
+    // });
+    // await file.save();
 
-  // // console.log(fs.readFileSync(join(__dirname, '..', path)))
-  // // console.log(join(__dirname, '..', path))
+    return res.status(201).send('File uploaded successfully.')
+  } catch (error) {
+    console.log(error)
 
-  // const file = new Image({
-  //   title,
-  //   description,
-  //   file_path: path,
-  //   file_mimetype: mimetype
-  // });
-  // await file.save();
-
-  // res.sendStatus(201).send('file uploaded successfully.');
-  // } catch (error) {
-  //   res.status(400).send('Error while uploading file. Try again later.');
-  // }
+    return res.status(400).send('Error while uploading file. Try again later.')
+  }
 }
